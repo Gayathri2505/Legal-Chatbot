f23a66907d83418d71fd3e5095eb856d42bf9da9
Gayathri2505 <116957331+Gayathri2505@users.noreply.github.com>
2025-08-07 09:32:35 +0530

    Update settings.py

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+DEBUG = os.getenv('DEBUG', '0')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['legal-chatbot-qnyq.onrender.com','127.0.0.1']
 
 
 # Application definition
